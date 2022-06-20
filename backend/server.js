14beb6691063046b56c73e2fe6b887d1cd769a14
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-06-20 22:12:47 +1000

    Added the not found middleware
    middleware added for server js for when route is not correct.

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 import express from 'express';
+import notFound from './middleware/not-found.js';
 const app = express();
 
 app.get('/', (req, res) => {
   res.send('Welcome!');
 });
+
+app.use(notFound);
 
 const port = process.env.PORT || 5000;
 
