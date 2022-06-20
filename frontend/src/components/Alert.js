c64661c1ab80145d8ef4058d0380c00e9593107c
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-06-20 19:51:32 +1000

    Added login/register page
    added login/register page with form row and alert component. set up index.js inside components folder for easier access to components.

---
+++ frontend/src/components/Alert.js
@@ -0,0 +1,5 @@
+const Alert = () => {
+  return <div className='alert alert-danger'>alert mssg</div>;
+};
+
+export default Alert;
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,7 +1,116 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import { Logo, FormRow, Alert } from '../components';
+// global context and useNavigate later
 
-const Register = () => {
-  return <div>Register</div>;
+const initialState = {
+  name: '',
+  email: '',
+  password: '',
+  isMember: true,
 };
+// if possible prefer local state
+// global state
+
+function Register() {
+  const [values, setValues] = useState(initialState);
+  //const [isLoading, setLoading] = useState(false);
+  //const [showAlert, setAlert] = useState(true);
+
+  // global context and useNavigate later
+
+  const handleChange = (e) => {
+    console.log(e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+  const toggleMember = () => {
+    setValues({ ...values, isMember: !values.isMember });
+  };
+
+  return (
+    <Wrapper className='page full-page'>
+      <div className='container'>
+        <form className='form' onSubmit={onSubmit}>
+          <Logo />
+          <h3>{values.isMember ? 'Login' : 'Register'}</h3>
+          {/* alert */}
+          {showAlert && <Alert />}
+          {/* name field */}
+          {!values.isMember && (
+            <FormRow
+              type='text'
+              name='name'
+              value={values.name}
+              handleChange={handleChange}
+            />
+          )}
+
+          {/* single form row */}
+          <FormRow
+            type='email'
+            name='email'
+            value={values.email}
+            handleChange={handleChange}
+          />
+          {/* end of single form row */}
+          {/* single form row */}
+          <FormRow
+            type='password'
+            name='password'
+            value={values.password}
+            handleChange={handleChange}
+          />
+          {/* end of single form row */}
+          <button type='submit' className='btn btn-block' disabled={isLoading}>
+            {isLoading ? 'Please wait...' : 'Submit'}
+          </button>
+          <p>
+            {values.isMember ? 'Not a member yet?' : 'Already a member?'}
+
+            <button type='button' onClick={toggleMember} className='member-btn'>
+              {values.isMember ? 'Register' : 'Login'}
+            </button>
+          </p>
+        </form>
+      </div>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled.section`
+  display: grid;
+  align-items: center;
+  .logo {
+    display: block;
+    margin: 0 auto;
+    margin-bottom: 1.38rem;
+  }
+  .form {
+    max-width: 400;
+    border-top: 5px solid var(--primary-500);
+  }
+  h3 {
+    text-align: center;
+  }
+  p {
+    margin: 0;
+    margin-top: 1rem;
+    text-align: center;
+  }
+  .btn {
+    margin-top: 1rem;
+  }
+  .member-btn {
+    background: transparent;
+    border: transparent;
+    color: var(--primary-500);
+    cursor: pointer;
+    letter-spacing: var(--letterSpacing);
+  }
+`;
 
 export default Register;
