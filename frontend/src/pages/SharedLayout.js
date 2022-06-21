071b113aa4ee5a69bd29e3a0fac65f5ca094254a
Zuhayer Alam <60056582+zuhayeralam@users.noreply.github.com>
2022-06-21 23:18:10 +1000

    Shared layout to show nested pages.

--- frontend/src/pages/SharedLayout.js
+++ frontend/src/pages/SharedLayout.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const SharedLayout = () => {
-  return <div>SharedLayout</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Link to='all-issues'>all issues</Link>
+        <Link to='add-issue'>add issue</Link>
+      </nav>
+      <Outlet />
+    </Wrapper>
+  );
 };
+
+const Wrapper = styled.section`
+  .dashboard {
+    display: grid;
+    grid-template-columns: 1fr;
+  }
+  .dashboard-page {
+    width: 90vw;
+    margin: 0 auto;
+    padding: 2rem 0;
+  }
+  @media (min-width: 992px) {
+    .dashboard {
+      grid-template-columns: auto 1fr;
+    }
+    .dashboard-page {
+      width: 90%;
+    }
+  }
+`;
 
 export default SharedLayout;
